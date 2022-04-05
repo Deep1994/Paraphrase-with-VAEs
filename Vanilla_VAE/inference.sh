80017e3e449507522d11385a3cd7cb0441046190
Ding Peng <1187174594@qq.com>
2022-04-05 15:22:20 +0800

    Update inference.sh

--- Vanilla_VAE/inference.sh
+++ Vanilla_VAE/inference.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=6 python inference.py \
-    -c /home/dingp/Paraphrase-with-VAEs/Vanilla_VAE/checkpoints/word_dropout_0.5/E10.pytorch \
+    -c /home/dingp/Paraphrase-with-VAEs/Vanilla_VAE/checkpoints/word_dropout_0.1/E10.pytorch \
     -n 5
